910497dc9277aaf644d27997535e55520075344c
Ezequiel Cavallo <ezequielcavallo02@gmail.com>
2025-03-18 11:29:27 -0300

    Primer commit F.P FRAGANCIAS

---
+++ app.js
@@ -0,0 +1,39 @@
+//Variables:es un espaciod de memoria reservado para almacenar información importante del programa.
+
+
+//Declaracion:
+let cliente;
+
+//Asignacion de valores:
+cliente = "Ezequiel";
+
+
+
+console.log(cliente);
+
+//Inicializar variables:
+
+let edad = 27;
+
+console.log(edad);
+
+//Constantes: Es una variable que una vez asignada no se puede modificar.
+
+const nacimiento = 1997;
+
+
+
+console.log(nacimiento);
+
+
+let suma = 230 + 100;
+
+console.log(suma)
+
+if (suma >30){
+    console.log("la suma es mayor que 30");
+}
+
+else{
+    console.log("la suma es menor o igual que 30");
+}
